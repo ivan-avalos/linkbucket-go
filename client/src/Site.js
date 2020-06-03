7e4cf2436b2a76f394db538cbcd4bd3a779ed47a
avalos <ivan.avalos.diaz@hotmail.com>
2020-06-02 23:50:08 -0500

    Changed Git repo from GitHub to Sourcehut

--- client/src/Site.js
+++ client/src/Site.js
@@ -16,37 +16,36 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import { Card, Form, Col, Button } from 'react-bootstrap';
-import { AppContext } from './AppProvider';
+import { Card } from 'react-bootstrap';
+import { Route } from 'react-router-dom';
 
 export default class Site extends React.Component {
     render() {
         return (
-			<div>
-				<Route path="/site/about">
-					<Card>
-						<Card.Header className="h5">
-							About
+            <div>
+                <Route path="/site/about">
+                    <Card>
+                        <Card.Header className="h5">
+                            About
 						</Card.Header>
-						<Card.Body className="lead">
-							Linkbucket is a free and open-source online bookmark manager focused on simplicity and minimalism.
+                        <Card.Body className="lead">
+                            Linkbucket is a free and open-source online bookmark manager focused on simplicity and minimalism.
 						</Card.Body>
-					</Card>
-				</Route>
-				<Route path="/site/open-source">
-					<Card>
-						<Card.Header className="h5">
-							Open Source
+                    </Card>
+                </Route>
+                <Route path="/site/open-source">
+                    <Card>
+                        <Card.Header className="h5">
+                            Open Source
 						</Card.Header>
-						<Card.Body className="lead">
-							The software that powers this website is called Linkbucket and anyone can download the&nbsp;
-							<a className="lead" href="https://github.com/ivan-avalos/linkbucket-go">source code</a> and run their own instance! 
+                        <Card.Body className="lead">
+                            The software that powers this website is called Linkbucket and anyone can download the&nbsp;
+							<a className="lead" href="https://git.sr.ht/~avalos/linkbucket-go">source code</a> and run their own instance!
 							It is written in Go and React and it is totally self-hostable, so you can run your own instance.
 						</Card.Body>
-					</Card>
-				</Route>
-			</div>
+                    </Card>
+                </Route>
+            </div>
         );
     }
 }
