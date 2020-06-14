de2699ce9e0c06421b2a2f559ca6c4b9ec2fe04c
avalos <ivan.avalos.diaz@hotmail.com>
2020-06-14 00:55:30 -0500

    The same UI improvements, but in Login and Register

--- client/src/Login.js
+++ client/src/Login.js
@@ -16,10 +16,10 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 import React from 'react';
-import { Card, Form, Col, Button } from 'react-bootstrap';
+import { Button, Card, Col, Container, Form } from 'react-bootstrap';
 import { AppContext } from './AppProvider';
-
 import ValidationErrors from './ValidationErrors';
+
 
 export default class Login extends React.Component {
     static contextType = AppContext;
@@ -49,43 +49,45 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <Card>
-                <Card.Header className="h5">Login</Card.Header>
-                <Card.Body>
-                    <ValidationErrors />
-                    <Form.Group className="row">
-                        <Form.Label className="col-md-4 col-form-label text-md-right">E-Mail Address</Form.Label>
-                        <Col md={6}>
-                            <Form.Control
-                                type="email"
-                                name="email"
-                                value={this.state.email}
-                                onChange={this.handleInput}
-                                required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="row">
-                        <Form.Label className="col-md-4 col-form-label text-md-right">Password</Form.Label>
-                        <Col md={6}>
-                            <Form.Control
-                                type="password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleInput}
-                                required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="row mb-0">
-                        <Col md={{span: 6, offset: 4}}>
-                            <Button 
-                                variant="primary"
-                                onClick={this.onSubmit}>
-                                Login
+            <Container>
+                <Card>
+                    <Card.Header className="h5">Login</Card.Header>
+                    <Card.Body>
+                        <ValidationErrors />
+                        <Form.Group className="row">
+                            <Form.Label className="col-md-4 col-form-label text-md-right">E-Mail Address</Form.Label>
+                            <Col md={6}>
+                                <Form.Control
+                                    type="email"
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={this.handleInput}
+                                    required />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="row">
+                            <Form.Label className="col-md-4 col-form-label text-md-right">Password</Form.Label>
+                            <Col md={6}>
+                                <Form.Control
+                                    type="password"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={this.handleInput}
+                                    required />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="row mb-0">
+                            <Col md={{ span: 6, offset: 4 }}>
+                                <Button
+                                    variant="primary"
+                                    onClick={this.onSubmit}>
+                                    Login
                             </Button>
-                        </Col>
-                    </Form.Group>
-                </Card.Body>
-            </Card>
+                            </Col>
+                        </Form.Group>
+                    </Card.Body>
+                </Card>
+            </Container>
         );
     }
 }
--- client/src/Register.js
+++ client/src/Register.js
@@ -15,11 +15,11 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-import React from 'react';
-import { Col, Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ValidationErrors from './ValidationErrors';
+import React from 'react';
+import { Button, Card, Col, Container, Form } from 'react-bootstrap';
 import { AppContext } from './AppProvider';
+import ValidationErrors from './ValidationErrors';
 
 export default class Register extends React.Component {
     static contextType = AppContext;
@@ -49,70 +49,72 @@ export default class Register extends React.Component {
         if (this.state.password === "" || this.state.password === this.state.confirm) {
             this.context.register(this.state);
         } else {
-            this.context.addError({"tag": "match"});
+            this.context.addError({ "tag": "match" });
         }
     }
 
     render() {
         return (
-            <Card>
-                <Card.Header className="h5">Register</Card.Header>
-                <Card.Body>
-                    <ValidationErrors errors={this.context.errors} />
-                    <Form.Group className="row">
-                        <Form.Label className="col-md-4 col-form-label text-md-right">Name</Form.Label>
-                        <Col md={6}>
-                            <Form.Control
-                                name="name"
-                                value={this.state.name}
-                                onChange={this.handleInput}
-                                required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="row">
-                        <Form.Label className="col-md-4 col-form-label text-md-right">E-Mail Address</Form.Label>
-                        <Col md={6}>
-                            <Form.Control
-                                type="email"
-                                name="email"
-                                value={this.state.email}
-                                onChange={this.handleInput}
-                                required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="row">
-                        <Form.Label className="col-md-4 col-form-label text-md-right">Password</Form.Label>
-                        <Col md={6}>
-                            <Form.Control
-                                type="password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleInput}
-                                required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="row">
-                        <Form.Label className="col-md-4 col-form-label text-md-right">Confirm Password</Form.Label>
-                        <Col md={6}>
-                            <Form.Control
-                                type="password"
-                                name="confirm"
-                                value={this.state.confirm}
-                                onChange={this.handleInput}
-                                required />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group className="row mb-0">
-                        <Col md={{span: 6, offset: 4}}>
-                            <Button 
-                                variant="primary"
-                                onClick={this.onSubmit}>
-                                Register
+            <Container>
+                <Card>
+                    <Card.Header className="h5">Register</Card.Header>
+                    <Card.Body>
+                        <ValidationErrors errors={this.context.errors} />
+                        <Form.Group className="row">
+                            <Form.Label className="col-md-4 col-form-label text-md-right">Name</Form.Label>
+                            <Col md={6}>
+                                <Form.Control
+                                    name="name"
+                                    value={this.state.name}
+                                    onChange={this.handleInput}
+                                    required />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="row">
+                            <Form.Label className="col-md-4 col-form-label text-md-right">E-Mail Address</Form.Label>
+                            <Col md={6}>
+                                <Form.Control
+                                    type="email"
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={this.handleInput}
+                                    required />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="row">
+                            <Form.Label className="col-md-4 col-form-label text-md-right">Password</Form.Label>
+                            <Col md={6}>
+                                <Form.Control
+                                    type="password"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={this.handleInput}
+                                    required />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="row">
+                            <Form.Label className="col-md-4 col-form-label text-md-right">Confirm Password</Form.Label>
+                            <Col md={6}>
+                                <Form.Control
+                                    type="password"
+                                    name="confirm"
+                                    value={this.state.confirm}
+                                    onChange={this.handleInput}
+                                    required />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="row mb-0">
+                            <Col md={{ span: 6, offset: 4 }}>
+                                <Button
+                                    variant="primary"
+                                    onClick={this.onSubmit}>
+                                    Register
                             </Button>
-                        </Col>
-                    </Form.Group>
-                </Card.Body>
-            </Card>
+                            </Col>
+                        </Form.Group>
+                    </Card.Body>
+                </Card>
+            </Container>
         );
     }
 }
